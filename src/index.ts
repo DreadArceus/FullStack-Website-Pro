d19d4c80c84f16f4a0e6fbe161754a6f109d54c2
dreadarceus <dreadarceus@gmail.com>
2021-02-21 01:40:38 +0530

    feat: express basic setup

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: "first post mic check" });
-  // await orm.em.persistAndFlush(post);
-
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send('hello');
+  })
+  app.listen(4000, () => {
+    console.log('server started on port 4000');
+  })
 };
 
 main().catch((err) => {
