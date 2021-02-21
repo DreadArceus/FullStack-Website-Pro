4ebc500b84d18a335f4694063798ac7bf8092b5e
dreadarceus <dreadarceus@gmail.com>
2021-02-21 12:13:43 +0530

    feat: GraphQL CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send('hello');
-  })
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
-    console.log('server started on port 4000');
-  })
+    console.log("server started on port 4000");
+  });
 };
 
 main().catch((err) => {
